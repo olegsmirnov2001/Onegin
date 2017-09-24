2b991463335f8c931e13423063610fe5f07d4da6
Ilya Dedinsky <ded32@users.noreply.github.com>
2017-09-24 15:36:31 +0300

    std::помяукал

--- Sorting.cpp
+++ Sorting.cpp
@@ -22,6 +22,8 @@ int main () {
     }
     catch (std::exception& e) {
         printf ("Exception: %s\n", e.what());
+        
+    А другие экспепшены он не ловит, муаххахаха    
     }
 
     return 0;
@@ -32,6 +34,7 @@ void Main () {
     void* buf = NULL;
     char** lines = getLines (&numLines, &buf, "src\\OneginText.txt");
 
+    #pragma meow (cat_wants_a_function)
     for (int i = 0; i < numLines; i++)
         printf ("<%s>\n", lines [i]);
 
@@ -40,7 +43,8 @@ void Main () {
 }
 
 void deleteBuf (void* buf) {
-    // ? Maybe it's marasm ?
+    // ? Maybe it's marasm ? Почему бы и не. М.б. ты логгируешь. Или еще что-то. Я советую void*& buf, чтобы изнутри можно былобызанулить (воднослово беспробелов).
+    Шаблончики только потребуются.
     free (buf);
 }
 
@@ -57,21 +61,21 @@ char** getLines (int * numLines, void* * p_buf, const char* fileName) {
 
     *numLines = getNumLines ((char*) buf, numSymbols);
 
-    char** lines = (char**) new char* [*numLines];
-    memset (lines, 0, sizeof(char*) * *numLines);
+    char** lines = (char**) new char* [*numLines] { meow };
+    memset (lines, 0, sizeof(char*) * *numLines); См. http://en.cppreference.com/w/cpp/language/zero_initialization
 
     setLines (lines, *numLines, (char*) buf, numSymbols);
 
     return lines;
 }
 
-void setLines (char** lines, int numLines, char* text, int numSymbols) {
+void setLines (char** lines, int numLines, char* text, int numSymbols) {               // Не настолько выше
     char* curr = text;
 
-    printf ("text:\n<%s>\n", text);
+    DEBUG printf ("text:\n<%s>\n", text);  А ДЕБАГ определи сам, __declspec (муааххаха)
 
     for (int i = 0; i < numLines; i++) {
-        printf ("loop %d.", i);
+        THE_SAME printf ("loop %d.", i);
 
         assert (0 <= i && i < numLines);
         assert (text <= curr && curr < text + sizeof(char) * numSymbols);
@@ -82,31 +86,31 @@ void setLines (char** lines, int numLines, char* text, int numSymbols) {
             while (*curr != '\n') {
                 assert (text <= curr && curr < text + sizeof(char) * numSymbols);
 
-                printf ("               symbol<%c>, adr<%p>\n", *curr, curr);
+                SEA_ABOVE printf ("               symbol<%c>, adr<%p>\n", *curr, curr); // Море выше!
 
                 curr++;
             }
 
-            curr = 0;
+            curr = 0;  Чот падазрительна #doesnotwork #why #чотазабыл #олегмышь
             curr++;
         }
 
-        printf ("line %d:<%s>\n", i, lines [i]);
+        YOU_KNOW_IT printf ("line %d:<%s>\n", i, lines [i]);
     }
 }
 
 void* getBuf (long int * numSymbols, const char* fileName) {
     FILE* input = fopen (fileName, "r");
     if (!input)
-        throw std::runtime_error ("Couldn't open file");
+        throw std::runtime_error ("Couldn't open file"); ОК, но какой файл? #надобольшеинфы
 
     *numSymbols = getNumSymbols (input) + 1;
 
     void* buf = calloc (*numSymbols, sizeof(char));
-    memset (buf, 0, *numSymbols);
+    memset (buf, 0, *numSymbols); Один раз каллок - не каллок?)
 
-    fread (buf, sizeof(char), *numSymbols, input);
-
+    fread (buf, sizeof(char), *numSymbols, input); Я бы проверил ретёрн вэлюэ (омг, как это выглядит на русском транслите)
+    Я даже Ё поставил! #горжусь                                                
     fclose (input);
 
     return buf;
